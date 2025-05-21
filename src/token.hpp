88160149dda0a2cc4e8aa24d7648d17d11d31921
GiveMe30Dollars <william80888088@gmail.com>
2025-05-22 04:11:19 +0800

    refactor3

--- src/token.hpp
+++ src/token.hpp
@@ -43,8 +43,71 @@ enum TokenType {
     _EOF
 };
 
-const std::unordered_map<TokenType,std::string> tokenTypeName;
-const std::unordered_map<std::string, TokenType> reservedKeywords;
+const std::unordered_map<TokenType,std::string> tokenTypeName = {
+    {LEFT_PAREN, "LEFT_PAREN"}, 
+    {RIGHT_PAREN, "RIGHT_PAREN"},
+    {LEFT_BRACE, "LEFT_BRACE"}, 
+    {RIGHT_BRACE, "RIGHT_BRACE"},
+
+    {COMMA, "COMMA"}, 
+    {DOT, "DOT"}, 
+    {MINUS, "MINUS"}, 
+    {PLUS, "PLUS"},
+    {SEMICOLON, "SEMICOLON"}, 
+    {SLASH, "SLASH"}, 
+    {STAR, "STAR"},
+
+    {BANG, "BANG"}, 
+    {BANG_EQUAL, "BANG_EQUAL"}, 
+    {EQUAL, "EQUAL"}, 
+    {EQUAL_EQUAL, "EQUAL_EQUAL"},
+    {GREATER, "GREATER"}, 
+    {GREATER_EQUAL, "GREATER_EQUAL"},
+    {LESS, "LESS"}, 
+    {LESS_EQUAL, "LESS_EQUAL"},
+
+    {IDENTIFIER, "IDENTIFIER"}, 
+    {STRING, "STRING"}, 
+    {NUMBER, "NUMBER"},
+
+    {AND,"AND"}, 
+    {CLASS,"CLASS"}, 
+    {ELSE,"ELSE"}, 
+    {FALSE,"FALSE"},
+    {FUN,"FUN"}, 
+    {FOR,"FOR"}, 
+    {IF,"IF"}, 
+    {NIL,"NIL"}, 
+    {OR,"OR"},
+    {PRINT,"PRINT"}, 
+    {RETURN,"RETURN"}, 
+    {SUPER,"SUPER"}, 
+    {THIS,"THIS"},
+    {TRUE,"TRUE"}, 
+    {VAR,"VAR"}, 
+    {WHILE,"WHILE"},
+    
+    {_EOF, "EOF"}
+};
+
+const std::unordered_map<std::string, TokenType> reservedKeywords = {
+    {"and", AND},
+    {"class", CLASS},
+    {"else", ELSE},
+    {"false", FALSE},
+    {"fun", FUN},
+    {"for", FOR},
+    {"if", IF},
+    {"nil", NIL},
+    {"or", OR},
+    {"print", PRINT},
+    {"return", RETURN},
+    {"super", SUPER},
+    {"this", THIS},
+    {"true", TRUE},
+    {"var", VAR},
+    {"while", WHILE},
+};
 
 bool isDigit(const char c);
 bool isAlpha(const char c);
