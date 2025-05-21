5e23a01af11019cae2398943c12dfcef81ae8d30
GiveMe30Dollars <william80888088@gmail.com>
2025-05-21 23:17:52 +0800

    strings5

--- src/scanner.hpp
+++ src/scanner.hpp
@@ -10,7 +10,27 @@
 
 #pragma once
 
-enum TokenType;
+enum TokenType {
+    // Single-character tokens.
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // One or two character tokens.
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // Literals.
+    IDENTIFIER, STRING, NUMBER,
+
+    // Keywords.
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    _EOF
+};
+
 std::string tokenTypeToString(const TokenType type);
 bool isDigit(const char c);
 bool isLetter(const char c);
