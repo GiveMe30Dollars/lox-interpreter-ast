ba73d58fd029e57444e159f55fbd1fd02fd22bfd
GiveMe30Dollars <william80888088@gmail.com>
2025-05-24 02:11:58 +0800

    test

--- src/parser.cpp
+++ src/parser.cpp
@@ -102,7 +102,9 @@ std::shared_ptr<Expr> Parser::equality(){
     while (match(Token::BANG_EQUAL, Token::EQUAL_EQUAL)){
         Token op = previous();
         std::shared_ptr<Expr> right = comparison();
+        std::cerr << "made other!\n";
         expr = std::make_shared<Binary>(expr, op, right);
+        std::cerr << "merged with other!\n";
     }
     return expr;
 }
