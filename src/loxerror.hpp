3bafa12b06128594a0419599e64ade18cc643349
GiveMe30Dollars <william80888088@gmail.com>
2025-05-24 02:02:27 +0800

    what

--- src/loxerror.hpp
+++ src/loxerror.hpp
@@ -11,7 +11,7 @@ class LoxError{
         std::string message;
         ScanError(int line, std::string message) : line(line), message(message) {}
         void print(void) {
-            std::cerr << "[line " << line << " ] Error: " << message << "\n";
+            std::cerr << "[line " << line << "] Error: " << message << "\n";
         }
     };
     class ParseError{
@@ -20,7 +20,7 @@ class LoxError{
         std::string message;
         ParseError(Token token, std::string message) : token(token), message(message) {}
         void print(void){
-            std::cerr << "[line " << token.line << " ] Error at ";
+            std::cerr << "[line " << token.line << "] Error at ";
             if (token.type == Token::_EOF) std::cerr << "end: ";
             else std::cerr << "'" << Token::tokenTypeName.at(token.type) << "': ";
             std::cerr << message << "\n";
@@ -32,7 +32,7 @@ class LoxError{
         std::string message;
         RuntimeError(Token token, std::string message) : token(token), message(message) {}
         void print(void){
-            std::cerr << "[line " << token.line << " ] Error at '" << Token::tokenTypeName.at(token.type) << "': " << message << "\n";
+            std::cerr << "[line " << token.line << "] Error at '" << Token::tokenTypeName.at(token.type) << "': " << message << "\n";
         }
     };
 };
