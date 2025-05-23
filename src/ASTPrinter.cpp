d6a9e8f9e59f2f6833e74125d7c0bb5ee7423238
GiveMe30Dollars <william80888088@gmail.com>
2025-05-24 02:21:36 +0800

    eval1

--- src/ASTPrinter.cpp
+++ src/ASTPrinter.cpp
@@ -1,17 +1,14 @@
 #include "ASTPrinter.hpp"
 
 std::string ASTPrinter::print(std::shared_ptr<Expr> expr){
-    std::cerr << "called print\n";
-    return std::any_cast<std::string>(expr);
+    return std::any_cast<std::string>(visit(expr));
 }
 
 std::any ASTPrinter::visit(std::shared_ptr<Expr> expr){
-    std::cerr << "called generic\n";
     return expr->accept(*this);
 }
 
 std::any ASTPrinter::visitLiteral(std::shared_ptr<Literal> curr){
-    std::cerr << "called literal\n";
     return curr->obj.toString(true);
 }
 
@@ -24,7 +21,6 @@ std::any ASTPrinter::visitUnary(std::shared_ptr<Unary> curr){
 }
 
 std::any ASTPrinter::visitBinary(std::shared_ptr<Binary> curr){
-    std::cerr << "called binary\n";
     return "(" + curr->op.lexeme + " " + print(curr->left) + " " + print(curr->right) + ")";
 }
 
