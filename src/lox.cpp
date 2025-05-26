4a0c33cda1a8afa9f8c4b60629018c0594c87730
GiveMe30Dollars <william80888088@gmail.com>
2025-05-26 21:54:43 +0800

    further debugging

--- src/lox.cpp
+++ src/lox.cpp
@@ -20,6 +20,7 @@ void Lox::run(std::string source, bool parseExpr){
     }
 
     ASTPrinter printer;
+    std::cerr << std::to_string(tokens.size()) << "\n";
     for (std::shared_ptr<Stmt> stmt : statements) std::cerr << printer.print(stmt) << "\n";
 
     try{
