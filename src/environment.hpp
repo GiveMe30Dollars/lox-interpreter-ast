d198809b9f8b1665d8a53a963dd77a7b93a13307
GiveMe30Dollars <william80888088@gmail.com>
2025-05-27 03:54:30 +0800

    allowed modification of variable in nested scope

--- src/environment.hpp
+++ src/environment.hpp
@@ -15,11 +15,22 @@ class Environment{
     Environment() {}
     Environment(std::shared_ptr<Environment> enclosing) : enclosing(enclosing) {}
     void define(Token& name, Object value){
+        // defines a variable and its value.
         values[name.lexeme] = value;
     }
     Object get(Token& name){
+        // gets a variable from this and enclosing scopes.
+        // value of closest scope returned.
+        // throws error if it doesn't exist.
         if (values.count(name.lexeme)) return values.at(name.lexeme);
         else if (enclosing) return enclosing->get(name);
+        else throw LoxError::RuntimeError(name, "Undefined variable '" + name.lexeme + "'");
+    }
+    void set(Token& name, Object value){
+        // sets the value of a variable in the closest enclosing scopes (incl. this)
+        // throws error if it doesn't exist.
+        if (values.count(name.lexeme)) values[name.lexeme] = value;
+        else if (enclosing) enclosing->set(name, value);
         else throw LoxError::RuntimeError(name, "Undefined variable '" + name.lexeme + "'");
     }
 };
