46cb1013f965d2d0f31bae2b0f9ed18a74861b5e
GiveMe30Dollars <william80888088@gmail.com>
2025-05-28 20:43:18 +0800

    resolved bugs

--- src/loxCallable.hpp
+++ src/loxCallable.hpp
@@ -1,7 +1,12 @@
 #include "token.hpp"
-#include "interpreter.hpp"
+
+// requires access to system clock for clock()
+#include <chrono>
 
 #pragma once
+
+// Interpreter not included: only declaration required
+class Interpreter;
 
 class LoxCallable{
     // Abstract class implementing a callable Lox object type
@@ -10,4 +15,18 @@ class LoxCallable{
         virtual int arity(void) = 0;
         virtual Object call(Interpreter& interpreter, std::vector<Object> arguments) = 0;
         virtual std::string toString(void) = 0;
+};
+
+class Clock : public LoxCallable{
+    public:
+    int arity(void) override { return 0; }
+    Object call(Interpreter& interpreter, std::vector<Object> arguments) override{
+        using namespace std::chrono;
+        return Object::number(
+            duration<double>(duration_cast<seconds>( system_clock::now().time_since_epoch() )).count()
+        );
+    }
+    std::string toString(void) override{
+        return "<native:clock>";
+    }
 };
