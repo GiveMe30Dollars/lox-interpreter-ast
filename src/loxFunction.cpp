7743117dc195c4cac5aea5b6ac086db74911e50f
GiveMe30Dollars <william80888088@gmail.com>
2025-05-28 23:48:09 +0800

    closure

--- src/loxFunction.cpp
+++ src/loxFunction.cpp
@@ -6,7 +6,7 @@ int LoxFunction::arity(){
 }
 
 Object LoxFunction::call(Interpreter& interpreter, std::vector<Object> arguments){
-    std::shared_ptr<Environment> env = std::make_shared<Environment>(interpreter.globals);
+    std::shared_ptr<Environment> env = std::make_shared<Environment>(closure);
     for (int i = 0; i < declaration->params.size(); i++){
         env->define(declaration->params[i], arguments[i]);
     }
--- src/loxFunction.hpp
+++ src/loxFunction.hpp
@@ -1,5 +1,6 @@
 #include "loxCallable.hpp"
 #include "stmt.hpp"
+#include "environment.hpp"
 
 #pragma once
 
@@ -8,7 +9,8 @@ class LoxFunction : public LoxCallable{
     // Wrapper for Function : Stmt
     public:
         std::shared_ptr<Function> declaration;
-        LoxFunction(std::shared_ptr<Function> declaration) : declaration(declaration) {}
+        std::shared_ptr<Environment> closure;
+        LoxFunction(std::shared_ptr<Function> declaration, std::shared_ptr<Environment> closure) : declaration(declaration), closure(closure) {}
         int arity(void) override;
         Object call(Interpreter& interpreter, std::vector<Object> arguments) override;
         std::string toString(void) override;
