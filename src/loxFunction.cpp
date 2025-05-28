4cf52f674f9910e56e6805dee4039783d9a5b430
GiveMe30Dollars <william80888088@gmail.com>
2025-05-29 04:06:13 +0800

    fixed bad call to Environment in LoxFunction

--- src/loxFunction.cpp
+++ src/loxFunction.cpp
@@ -8,7 +8,7 @@ int LoxFunction::arity(){
 Object LoxFunction::call(Interpreter& interpreter, std::vector<Object> arguments){
     std::shared_ptr<Environment> env = std::make_shared<Environment>(closure);
     for (int i = 0; i < declaration->params.size(); i++){
-        env->define(declaration->params[i], arguments[i]);
+        env->define(declaration->params[i].lexeme, arguments[i]);
     }
     try{
         interpreter.executeBlock(declaration->body, env);
