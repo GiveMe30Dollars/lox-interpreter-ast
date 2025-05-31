a0e2eba4b7f4caa554c0359343e3073fdaf4b73f
GiveMe30Dollars <william80888088@gmail.com>
2025-05-31 21:58:55 +0800

    modified REPL exit clause

--- src/main.cpp
+++ src/main.cpp
@@ -85,11 +85,11 @@ int main(int argc, char *argv[]) {
         return 0;
     }
     if (command == "repl"){
-        std::cout << " \n-----LOX REPL-----\nTo exit, type exit().\n";
+        std::cout << " \n-----LOX REPL-----\nTo exit, type 'exit'.\n";
         while(true){
             std::string replInput = "";
             std::getline(std::cin, replInput);
-            if (replInput == "exit()") break;
+            if (replInput == "exit") break;
             Lox::run(replInput, true);
             if (Lox::hasCompileError) std::cout << "returned code: 65\n";
             if (Lox::hasRuntimeError) std::cout << "returned code: 70\n";
